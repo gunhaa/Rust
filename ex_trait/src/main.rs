25cc4c64d2a2012cc0d224806a8401c6714a7d78
gunha <wh8299@gmail.com>
2025-04-12 16:08:18 +0900

    feat : trait 추가

--- ex_trait/src/main.rs
+++ ex_trait/src/main.rs
@@ -1,3 +1,22 @@
+// 프로젝트 이름
+use ex_trait::{Summary, Tweet};
+
 fn main() {
-    println!("Hello, world!");
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false, 
+        retweet: false,
+    };
+
+    println!("1 new tweet : {}" , tweet.summarize());
+
+    notify(&tweet);
+}
+
+pub fn notify(item: &impl Summary){
+    print!("Breaking news! {}" , item.summarize())
 }
